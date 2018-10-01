7a0e0caba08bbb64c731845b800a53a75355fffb
tanyue <417045639@qq.com>
2018-10-01 15:09:36 +0800

    Atom翻译插件
    Atom翻译插件

---
+++ spec/ty-package-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TyPackageView from '../lib/ty-package-view';
+
+describe('TyPackageView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
